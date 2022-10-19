6b0a46f6e2a473add418b60921c1c168754005ea
Carl Christiansen <eaachchr@students.eaaa.dk>
2022-10-19 11:15:47 +0200

    tabs for bids

--- artem-frontend/src/pages/Bids.jsx
+++ artem-frontend/src/pages/Bids.jsx
@@ -1,8 +1,14 @@
 import React from "react";
 import Header from "../components/Header";
+import ButtonTabs from "../components/ButtonTabs";
 
 function Bids() {
-  return <Header text="Bids" />;
+  return (
+    <div>
+      <Header text="Bids" />
+      <ButtonTabs></ButtonTabs>
+    </div>
+  );
 }
 
 export default Bids;
