21bc189aa5c6f4f57b15a9287835b031324bd4ef
Katerina Kolarova <89975146+Poc-a-hontas@users.noreply.github.com>
2022-10-19 10:15:08 +0200

    account page- adding content

--- artem-frontend/src/pages/Account.jsx
+++ artem-frontend/src/pages/Account.jsx
@@ -2,21 +2,58 @@ import { Button, Typography } from "@mui/material";
 import { Container } from "@mui/system";
 import React from "react";
 import Header from "../components/Header";
+import IconButton from '@mui/material/IconButton';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import AlternateEmailIcon from '@mui/icons-material/AlternateEmail';
+
 
 export default function Account() {
   return (
     <>
       <Header text="Account" withAccountOptions />
       <Container>
+        <img src="account-img.png" alt="profile pic" />
         <Typography>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi,
-          deleniti cumque culpa veritatis doloribus quos ratione necessitatibus
-          quia aliquam exercitationem.
+          Johanna Ark
         </Typography>
+
+        <IconButton aria-label="linkedin page">
+        <LinkedInIcon/>
+        </IconButton>
+
+        <IconButton aria-label="instagram page">
+        <InstagramIcon/>
+        </IconButton>
+
+
+        <IconButton  aria-label="email">
+        <AlternateEmailIcon/>
+        </IconButton>
+
+        
+           
+        <Typography>
+        There are no mistakes in art, just happy little accidents. 📍A freelance copywriter based in Aalborg. 🎨Hungry for visuals. 
+        </Typography>
+
       </Container>
-      <Button color="primary" size="small">
-        Something
-      </Button>
+      <Button
+          variant="contained"
+          disableElevation
+        > Follow
+        </Button>
+      {/* <Button
+          sx={{ mt: theme.spacing(2) }}
+          variant="contained"
+          size="large"
+          type="submit"
+          disabled={loading}
+          fullWidth
+          disableElevation
+        >
+          {loading ? <CircularProgress size={14} /> : capitalize(type)}
+        </Button> */}
     </>
   );
 }
