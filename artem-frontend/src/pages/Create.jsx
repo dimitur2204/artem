361a9160f1bb8ed907f92f29b60ac66424063dc6
Katerina Kolarova <89975146+Poc-a-hontas@users.noreply.github.com>
2022-10-20 14:37:56 +0200

    creating NewPost page

--- artem-frontend/src/pages/Create.jsx
+++ artem-frontend/src/pages/Create.jsx
@@ -1,8 +1,24 @@
 import React from "react";
 import Header from "../components/Header";
+import { Button, Typography } from "@mui/material";
+import { Container } from "@mui/system";
+import CloseIcon from '@mui/icons-material/Close';
+import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
+import ToggleOffOutlinedIcon from '@mui/icons-material/ToggleOffOutlined';
+import ToggleOnOutlinedIcon from '@mui/icons-material/ToggleOnOutlined';
 
 function Create() {
-  return <Header text="Create" />;
+  return <Header text="New Post" />;
 }
+<Container>
 
+<Button
+variant="contained"
+disableElevation
+> Post
+</Button>
+
+
+</Container>
 export default Create;
+
