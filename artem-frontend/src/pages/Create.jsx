eea19b7cd9266efe2f802c7f49d68fe976d80aad
Katerina Kolarova <89975146+Poc-a-hontas@users.noreply.github.com>
2022-10-20 16:50:29 +0200

    Create.js adding icons

--- artem-frontend/src/pages/Create.jsx
+++ artem-frontend/src/pages/Create.jsx
@@ -1,24 +1,64 @@
 import React from "react";
 import Header from "../components/Header";
-import { Button, Typography } from "@mui/material";
-import { Container } from "@mui/system";
+import {
+  InputAdornment,
+  Container,
+  IconButton,
+  Box,
+  Button
+} from "@mui/material";
 import CloseIcon from '@mui/icons-material/Close';
 import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
 import ToggleOffOutlinedIcon from '@mui/icons-material/ToggleOffOutlined';
 import ToggleOnOutlinedIcon from '@mui/icons-material/ToggleOnOutlined';
+import Input from "../components/Input";
 
 function Create() {
-  return <Header text="New Post" />;
+  return(
+  <>
+  <Header text="New Post" />
+  <IconButton position="end" style={{color:"#000"}} aria-label="cross">
+            <CloseIcon/>
+      </IconButton>
+    <Container>
+
+      <IconButton sx={{display:"flex"}} color="primary" aria-label="plus symbol">
+            <AddCircleOutlineIcon sx={{ fontSize:"9rem", justifyContent:"center"}}/>
+      </IconButton>
+
+      <Input placeholder="Title"
+          endAdornment={
+            <InputAdornment>
+      </InputAdornment>
+                }
+                />
+
+      <Input style={{padding: "1rem", marginTop: "20px"}} placeholder="Item description"
+          endAdornment={
+            <InputAdornment>
+      </InputAdornment>
+                }
+                />
+
+
+      <IconButton style={{color:"#000",}} aria-label="switcher">
+            <ToggleOnOutlinedIcon/>
+      </IconButton>
+
+      {/* <IconButton style={{color:"#000"}} aria-label="switcher">
+            <ToggleOffOutlinedIcon/>
+      </IconButton> */}
+
+      <Button
+          variant="contained"
+          size="large"
+          disableElevation
+      > Post
+      </Button>
+
+    </Container>
+</>
+);
 }
-<Container>
-
-<Button
-variant="contained"
-disableElevation
-> Post
-</Button>
-
-
-</Container>
 export default Create;
 
