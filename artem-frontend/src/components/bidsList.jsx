2bf7be528624d49d22a027dd0082dfd3afba9d19
Carl Christiansen <eaachchr@students.eaaa.dk>
2022-10-23 21:29:29 +0200

    Bids and watched section (not working yet)

---
+++ artem-frontend/src/components/bidsList.jsx
@@ -0,0 +1,27 @@
+const itemData = [
+    {
+      img: 'fox.png',
+      title: 'The fox wanderer',
+      author: 'Current bid <br> 170.00$ <br> 2 hours left',
+    },
+    {
+      img: 'dream.png',
+      title: 'Catcher of dreams',
+      author: 'Current bid <br> 356.00$ <br> 4 hours left',
+    },
+    {
+      img: 'coaster.png',
+      title: 'Coral Coaster',
+      author: 'Current bid <br> 54.00$ <br> 9 hours left',
+    },
+    {
+      img: 'snail.png',
+      title: 'Snail Overboard',
+      author: 'Current bid <br> 31.00$ <br> 10 hours left',
+    },
+    {
+      img: 'holder.png',
+      title: 'Plant Holder',
+      author: 'Current bid <br> 61.00$ <br> 12 hours left',
+    },
+  ];
