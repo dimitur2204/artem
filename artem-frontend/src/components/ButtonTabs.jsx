51f0f2a698e9d5b6780009b4da04dc00ab4be647
Carl Christiansen <eaachchr@students.eaaa.dk>
2022-10-24 09:20:10 +0200

    bids added

--- artem-frontend/src/components/ButtonTabs.jsx
+++ artem-frontend/src/components/ButtonTabs.jsx
@@ -5,9 +5,9 @@ import Tab from "@mui/material/Tab";
 import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
 import "./ButtonTabs.css";
-import ImageList from '@mui/material/ImageList';
-import ImageListItem from '@mui/material/ImageListItem';
-import ImageListItemBar from '@mui/material/ImageListItemBar';
+import ImageList from "@mui/material/ImageList";
+import ImageListItem from "@mui/material/ImageListItem";
+import ImageListItemBar from "@mui/material/ImageListItemBar";
 import itemData from "./bidsList";
 
 function TabPanel(props) {
@@ -63,34 +63,25 @@ export default function BasicTabs() {
         </Tabs>
       </Box>
 
-
       <TabPanel value={value} index={0}>
-        
-       {/* export default function TitlebarBelowImageList() {
-  return  (
-    <ImageList sx={{ width: 500, height: 450 }}>
-      {itemData.map((item) => (
-        <ImageListItem key={item.img}>
-          <img
-            src={`${item.img}?w=248&fit=crop&auto=format`}
-            srcSet={`${item.img}?w=248&fit=crop&auto=format&dpr=2 2x`}
-            alt={item.title}
-            loading="lazy"
-          />
-          <ImageListItemBar
-            title={item.title}
-            subtitle={<span>by: {item.author}</span>}
-            position="below"
-          />
-        </ImageListItem>
-      ))}
-    </ImageList>
-  );
-} 
-*/}
-          
+        <ImageList sx={{ width: 500, height: 450 }}>
+          {itemData.map((item) => (
+            <ImageListItem key={item.img}>
+              <img
+                src={`${item.img}?w=248&fit=crop&auto=format`}
+                srcSet={`${item.img}?w=248&fit=crop&auto=format&dpr=2 2x`}
+                alt={item.title}
+                loading="lazy"
+              />
+              <ImageListItemBar
+                title={item.title}
+                subtitle={<span>by: {item.author}</span>}
+                position="below"
+              />
+            </ImageListItem>
+          ))}
+        </ImageList>
       </TabPanel>
-
 
       <TabPanel value={value} index={1}>
         Item Two
@@ -98,4 +89,3 @@ export default function BasicTabs() {
     </Box>
   );
 }
-
