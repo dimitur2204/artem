a9acb2d007af5c3224897416b6ebeec79ad355f6
dimitar.nizamov <dimitur2204@gmail.com>
2022-10-27 19:48:42 +0200

    Add landing page on first login

--- artem-frontend/src/App.js
+++ artem-frontend/src/App.js
@@ -3,8 +3,15 @@ import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import "./index.css";
 import Layout from "./components/Layout";
+import { useNavigate } from "react-router-dom";
 
 function App({ children }) {
+  const navigate = useNavigate();
+  React.useEffect(() => {
+    if (localStorage.getItem('firstLogin')) return
+    localStorage.setItem('firstLogin', true)
+    navigate('/landing')
+  })
   return (
     //defines the layout of the application. reusable on every page. in our case, it defines the layout of the nav bar
     <Layout>
